e8a4ff031039c6e5f36e928614c570bb47fe86a1
SBT-Starovoytov-AV <AVStarovoytov.SBT@sberbank.ru>
2017-12-05 14:28:09 +0300

    Простой Hello World

---
+++ Spring/src/main/java/ConsoleHello/MainClass.java
@@ -0,0 +1,20 @@
+package ConsoleHello;
+
+/**
+ * Стартовый класс для консольного "hello world" приложения
+ *
+ * @author Starovoytov
+ * @since 05.12.2017
+ */
+public class MainClass
+{
+	/**
+	 * Вывод "Hello World!" в консоль
+	 *
+	 * @param args Не используется
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
