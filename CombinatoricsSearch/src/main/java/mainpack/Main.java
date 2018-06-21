33088fc316af65ee686a0c858b4b151212ecea4d
SBT-Starovoytov-AV <AVStarovoytov.SBT@sberbank.ru>
2018-06-21 07:59:57 +0300

    Инициализация проекта.

---
+++ CombinatoricsSearch/src/main/java/mainpack/Main.java
@@ -0,0 +1,13 @@
+package mainpack;
+
+/**
+ * Запуск приложения
+ *
+ * @author Starovoytov
+ * @since 29.11.2017
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
