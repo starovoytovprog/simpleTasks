08549b81ab656aab28adc5154d675ea798988565
StarovoytovProg <adminofusa@gmail.com>
2018-09-25 10:30:09 +0300

    Реализация метода копирования.

--- FileRandomizer/src/main/java/mainpack/MainClass.java
+++ FileRandomizer/src/main/java/mainpack/MainClass.java
@@ -1,6 +1,18 @@
 package mainpack;
 
+/**
+ * Стартовый класс утилиты
+ *
+ * @author Starovoytov
+ * @since 24.09.2018
+ */
 public class MainClass {
+
+	/**
+	 * Запуск утилиты
+	 *
+	 * @param args Не используется
+	 */
 	public static void main(String[] args) {
 		System.out.println("Ok");
 	}
---
+++ FileRandomizer/src/main/java/rand/Randomizer.java
@@ -0,0 +1,56 @@
+package rand;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Класс для копирования файлов
+ *
+ * @author Starovoytov
+ * @since 24.09.2018
+ */
+public class Randomizer {
+
+	/**
+	 * Копирование файлов
+	 *
+	 * @param source      директория с копируемыми файлами
+	 * @param destination директория назначения
+	 */
+	public static void CopyAll(String source, String destination) throws IOException {
+		clearDestinationPath(destination);
+
+		List<File> files = Arrays.asList(new File(source).listFiles());
+		Collections.shuffle(files);
+
+		for (File file : files) {
+			Files.copy(file.toPath(), new File(destination + "\\" + file.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+		}
+	}
+
+	/**
+	 * Очистка каталога
+	 *
+	 * @param destination каталог
+	 * @throws IOException исключения ввода-вывода при обращении к файлам
+	 */
+	private static void clearDestinationPath(String destination) throws IOException {
+		Files.walkFileTree(Paths.get(destination), (new DeleteVisitor()));
+	}
+
+	/**
+	 * Класс для очистки каталога
+	 */
+	private static class DeleteVisitor extends SimpleFileVisitor<Path> {
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			Files.delete(file);
+			return FileVisitResult.CONTINUE;
+		}
+	}
+}
