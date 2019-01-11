b50c024169ae07f26a28ef436d5c7fe1085918a8
StarovoytovProg <adminofusa@gmail.com>
2019-01-11 09:10:10 +0300

    Вывод версии сборки для HelloWorld

--- HelloWorld/src/main/java/mainpack/HelloWorldMain.java
+++ HelloWorld/src/main/java/mainpack/HelloWorldMain.java
@@ -14,5 +14,9 @@ public class HelloWorldMain {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		String version = HelloWorldMain.class.getPackage().getImplementationVersion();
+		if (version != null) {
+			System.out.println("Build info: " + version);
+		}
 	}
 }
